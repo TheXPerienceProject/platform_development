b08b2db83f2ae07130a4aabea4f827b9ab0a47f9
Rob Seymour <rseymour@google.com>
2022-03-25 18:28:01 +0000

    Do not install host snapshot modules with existing prebuilts.
    Host fake snapshot includes a prebuilt attribute for a host module
    that already contains a prebuilt version in the source tree.  When
    installing the host snapshot ignore any modules that already contains
    a prebuilt version.
    
    Bug: 225890931
    Test: m HOST_FAKE_SNAPSHOT_ENABLE=true host-fake-snapshot dist
    Test: install fake snapshot with update.py and validate prebuilt modules are not included
    Change-Id: Ia8bb44c00ad5fc448ab9110d840d9059ae3294f9
    (cherry picked from commit 01a4f9b2e317a0e945d4ddfb620834315902778f)

--- vendor_snapshot/update.py
+++ vendor_snapshot/update.py
@@ -112,6 +112,7 @@ JSON_TO_BP = {
     'RuntimeLibs': 'runtime_libs',
     'Required': 'required',
     'Filename': 'filename',
+    'Prebuilt': 'prebuilt',
 }
 
 SANITIZER_VARIANT_PROPS = {
@@ -407,11 +408,15 @@ def convert_json_host_data_to_bp(mod, install_dir):
     All host modules are created as a cc_prebuilt_binary
     blueprint module with the prefer attribute set to true.
 
+    Modules that already have a prebuilt are not created.
+
     Args:
       mod: JSON definition of the module
       install_dir: installation directory of the host snapshot
     """
     prop = convert_json_data_to_bp_prop(mod, install_dir)
+    if 'prebuilt' in prop:
+        return
     prop['host_supported'] = True
     prop['device_supported'] = False
     prop['prefer'] = True
@@ -443,7 +448,8 @@ def gen_host_bp_file(install_dir):
                     props = json.load(rfp)
                     for mod in props:
                         prop = convert_json_host_data_to_bp(mod, install_dir)
-                        wfp.write(prop)
+                        if prop:
+                            wfp.write(prop)
 
 def gen_bp_files(image, vndk_dir, install_dir, snapshot_version):
     """Generates Android.bp for each archtecture.
@@ -651,7 +657,7 @@ def check_host_usage(install_dir, ninja_binary, ninja_file, goals, output):
     with open(output, 'w') as f:
         f.write('vsdk_host_tools = [ \n')
         for m in sorted(used_modules):
-            f.write('  "%s", \n' % m)
+            f.write('  "%s",\n' % m)
         f.write('] \n')
 
 def check_module_usage(install_dir, ninja_binary, image, ninja_file, goals,
